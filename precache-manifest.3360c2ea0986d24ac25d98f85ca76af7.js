5154b9d1c15fbbcf969841f6c4347973c3497a2e
Albert Gabdullin <albert.gabdullin@piano.io>
2020-02-10 00:51:08 +0300

    Updates

---
+++ precache-manifest.3360c2ea0986d24ac25d98f85ca76af7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45daa94713aa9353d27abd3189429925",
+    "url": "/party/index.html"
+  },
+  {
+    "revision": "002ded83b87984cd177a",
+    "url": "/party/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "628674538a7ac07c3517",
+    "url": "/party/static/js/2.2ab7c311.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/party/static/js/2.2ab7c311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002ded83b87984cd177a",
+    "url": "/party/static/js/main.8088fc9a.chunk.js"
+  },
+  {
+    "revision": "6ab82fe46059374923e3",
+    "url": "/party/static/js/runtime-main.f9870149.js"
+  }
+]);
