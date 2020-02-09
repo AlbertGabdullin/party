91d17906edfb6e10eae380fdabae1cd1ea9103b6
Albert Gabdullin <albert.gabdullin@piano.io>
2020-02-10 00:42:56 +0300

    add testing libraries and configure enzyme

--- src/containers/Party/Party.test.tsx
+++ src/containers/Party/Party.test.tsx
@@ -1,5 +1,9 @@
+import React from 'react';
 import { MockedProvider } from '@apollo/react-testing';
 import { GET_PARTY } from '../../queries';
+import { mount } from 'enzyme';
+import Party from './index';
+import Person from '../../components/Person';
 
 const mocksWithEmptyParty = [
   {
@@ -25,3 +29,46 @@ const mocksWithEmptyParty = [
   },
 ];
 
+const mocks = [
+  {
+    request: {
+      query: GET_PARTY,
+    },
+    result: {
+      data: {
+        party: {
+          rick: {
+            id: '1',
+            name: 'Rick Sanchez',
+            image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+          },
+          morty: {
+            id: '2',
+            name: 'Morty Smith',
+            image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+          },
+        },
+      },
+    },
+  },
+];
+
+describe('test renders', () => {
+  test('with empty party should be renderer without errors', () => {
+    const party = mount(
+      <MockedProvider mocks={mocksWithEmptyParty}>
+        <Party />
+      </MockedProvider>,
+    );
+    expect(party.find(Person).length).toEqual(2);
+  });
+
+  test('with real data should be renderer without errors', () => {
+    const party = mount(
+      <MockedProvider mocks={mocks}>
+        <Party />
+      </MockedProvider>,
+    );
+    expect(party.find(Person).length).toEqual(2);
+  });
+});
